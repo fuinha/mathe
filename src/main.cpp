d9532c5d5ff3abaadbadf78421c6a41a7722da7f
Alexandre Rodrigo Pinheiro <alexandrerodrigopinheiro@gmail.com>
2019-01-15 15:00:59 -0200

    Update class test

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,18 @@
 
 #include <iostream>
 #include "mathe.h"
+#include "bank.h"
 
 int main(int argc, char **argv) {
+	Mathe mMath;
+	Bank mBank;
+	mBank.init(100.0, 200.0);
+	mBank.addInput(100.0);
+	mBank.addOutput(100.0);
+	mBank.updateGame();
+	std::cout << mBank.isVeryHard() << std::endl;
+	std::cout << mBank.getInput() << std::endl;
+	std::cout << mBank.getOutput() << std::endl;
 	std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
 	return 0;
 }
